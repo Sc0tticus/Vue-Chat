7383c4d46b089ba7e60d76c2d52233bee84fdd5f
Sc0tticus <ssinger303@gmail.com>
2020-09-07 15:42:23 -0600

    CH60 6S! Added firebase folder w/init.js and firebaseConfig object

---
+++ src/firebase/init.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyA7Fvwrcg8FqCgmx2gkoDZ_OmXvVsmEoRY",
+  authDomain: "vue-chat-45591.firebaseapp.com",
+  databaseURL: "https://vue-chat-45591.firebaseio.com",
+  projectId: "vue-chat-45591",
+  storageBucket: "vue-chat-45591.appspot.com",
+  messagingSenderId: "780070974640",
+  appId: "1:780070974640:web:11b58e84085924d48c7775",
+  measurementId: "G-ETM0L4S32S"
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebaseApp.firestore().settings({ timestampsInSnapshots: true })
+firebase.analytics();
+
+export default firebaseApp.firestore()
