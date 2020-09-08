63676ed5949444f678e5632145ea7292dd7f3e70
Sc0tticus <ssinger303@gmail.com>
2020-09-07 18:04:20 -0600

    CH67 done. Added real-time chat functionality. Time-stamps for chat messages are not working currently

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,10 +3,10 @@
     <h2 class="center teal-text">Vue Chat </h2>
     <div class="card-content">
       <ul class="messages">
-        <li>
-          <span class="teal-text">Name</span>
-          <span class="gray-text text-darken-3">message</span>
-          <span class="grey-text time">time</span>
+        <li v-for="message in messages" :key="message.id">
+          <span class="teal-text">{{ message.name }}</span>
+          <span class="gray-text text-darken-3">{{ message.content }}</span>
+          <span class="grey-text time">{{ message.timestamp }}</span>
         </li>
       </ul>
     </div>
@@ -19,6 +19,8 @@
 
 <script>
 import NewMessage from '@/components/NewMessage'
+import db from '@/firebase/init'
+
 export default {
   name: 'Chat',
   props: ['name'],
@@ -27,8 +29,26 @@ export default {
   },
   data(){
     return {
-
+      messages: []
     }
+  },
+  created(){
+    let ref = db.collection('messages')
+    // .orderBy('timestamp')
+
+    ref.onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(change => {
+        if(change.type == 'added'){
+          let doc = change.doc
+          this.messages.push({
+            id: doc.id,
+            name: doc.data().name,
+            content: doc.data().content,
+            timestamp: doc.data().timestamp
+          })
+        }
+      })
+    })
   }
 }
 </script>
